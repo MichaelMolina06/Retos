243dc06a72e4e98490804e301916cdbeabcdf65e
Michael Molina <m-steven_97@hotmail.com>
2022-05-03 21:42:38 -0500

    Declaracion e ingreso de variables, u-c-t

--- reto_cdt.py
+++ reto_cdt.py
@@ -7,9 +7,11 @@ def run():
     print("Bienvenido a su CDT")
 
     print("A continuacion ingrese los siguientes datos:")
-    print("Nombre de Usuario")
-    print("Monto o capital")
-    print("Tiempo transcurrido en meses")
+    u=input(str(("Nombre de Usuario")))
+    c=input(int(("Monto o capital")))
+    t=input(int(("Tiempo transcurrido en meses")))
+
+    CDT(u,c,t)
 
 if __name__ == '__main__':
     run()

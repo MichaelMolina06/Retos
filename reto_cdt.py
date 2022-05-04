dd1804f1fe75b6bc5ac67d08a9c19656e3baabc7
Michael Molina <m-steven_97@hotmail.com>
2022-05-03 21:36:03 -0500

    Definir funciones

--- reto_cdt.py
+++ reto_cdt.py
@@ -1,9 +1,15 @@
 
-def CDT():
+def CDT(usuario, capital, tiempo):
     pass
 
 def run():
-    pass
+    
+    print("Bienvenido a su CDT")
+
+    print("A continuacion ingrese los siguientes datos:")
+    print("Nombre de Usuario")
+    print("Monto o capital")
+    print("Tiempo transcurrido en meses")
 
 if __name__ == '__main__':
     run()

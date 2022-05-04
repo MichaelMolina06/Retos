5c3f4db14aed74d93c6b916e172af71bd20979ba
Michael Molina <m-steven_97@hotmail.com>
2022-05-03 20:34:18 -0500

    Creacion de archivo reto_cdt.py

---
+++ reto_cdt.py
@@ -0,0 +1,4 @@
+''' 
+Descripcion del problema: Una entidad Bancaria o entidad financiera, requiere calcular el valor de los intereses ganados y el total final de dinero para diferentes clientes, de acuerdo, a una cantidad de dinero invertida en un CDT. Un CDT por su parte, es un producto financiero que ofrece la posibilidad de guardar dinero durante un tiempo determinado para recibir posteriormente sus intereses devengados, asimismo, ofrece rendimientos a una tasa fija, asegurando una rentabilidad libre de riesgo en un tiempo determinado que por lo general debe ser mayor a dos (2) meses. Si, este dinero se retira antes de este periodo se aplica una penalidad del 2%.
+En ese sentido, el valor de los intereses ganados por un periodo de tiempo superior a dos meses se determina a traves de una formula.
+'''

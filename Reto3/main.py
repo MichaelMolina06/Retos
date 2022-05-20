3bb654073323ab3ed7ee424f3d3e9d2c63039aa8
varcmis <105815670+varcmis@users.noreply.github.com>
2022-05-20 00:11:42 -0500

    scriptPyReto3

--- Reto3/main.py
+++ Reto3/main.py
@@ -1,6 +1,5 @@
 import Autopartes 
-(Autopartes.consultaRegistro(Autopartes.AutoPartes([
- (2001,'rosca', 'PT29872',2,45,'Luis Molero',3456,'12/06/2020'),
+(Autopartes.consultaRegistro(Autopartes.AutoPartes([(2001,'rosca', 'PT29872',2,45,'Luis Molero',3456,'12/06/2020'),
  (2010,'bujía', 'MS9512',4,15,'Carlos Rondon',1256,'12/06/2020'),
  (2010,'bujía', 'ER6523',9,36,'Pedro Montes',1243,'12/06/2020'),
  (3578,'tijera', 'QW8523',1,128,'Pedro Faria',1456,'12/06/2020'),
